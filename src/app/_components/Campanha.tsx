14b2c6d0a4c7e79efba4f4adb4570677c5c83a3e
silvakwan1 <kauans003@gmail.com>
2024-11-03 22:18:19 -0300

    ajustei um bug

--- src/app/_components/Campanha.tsx
+++ src/app/_components/Campanha.tsx
@@ -4,15 +4,22 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { CardCampanha } from "./CardCampanha";
 
+// Definição da interface para a promoção
+interface Promocao {
+  title: string;
+  image: {
+    data: string; // Aqui, 'data' é o formato que o seu backend está enviando
+  };
+  timeEnd: string; // Ajuste o tipo se necessário
+}
+
 function Campanha() {
-  const [location, setLocation] = useState<GeolocationPosition | null>(null);
-  const [promocoes, setPromocoes] = useState<any[]>([]);
+  const [promocoes, setPromocoes] = useState<Promocao[]>([]);
 
   useEffect(() => {
     if (typeof window !== "undefined" && "geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setLocation(position);
           getData(position.coords.latitude, position.coords.longitude);
         },
         (error) => console.error(error)
@@ -22,16 +29,18 @@ function Campanha() {
 
   async function getData(latitude: number, longitude: number) {
     try {
-      const response = await axios(
+      const response = await axios.get<Promocao[]>(
         `http://localhost:3333/promocoes?latitude=${latitude}&longitude=${longitude}`
       );
 
       // Converte o Buffer em Base64
-      const promocoesComImagemBase64 = response.data.map((promocao: any) => ({
+      const promocoesComImagemBase64 = response.data.map((promocao) => ({
         ...promocao,
-        image: `data:image/png;base64,${Buffer.from(
-          promocao.image.data
-        ).toString("base64")}`,
+        image: {
+          data: `data:image/png;base64,${Buffer.from(
+            promocao.image.data
+          ).toString("base64")}`,
+        },
       }));
 
       console.log(promocoesComImagemBase64);
@@ -48,7 +57,7 @@ function Campanha() {
           <CardCampanha
             key={index}
             Alt={promocao.title}
-            Src={promocao.image}
+            Src={promocao.image.data} // Corrigido para passar a string correta
             timeEnd={promocao.timeEnd}
             title={promocao.title}
           />
